4a796548a20e9ca9065b732ad9cfaabc930d9425
GabrielViana01 <gabriel.iee@hotmail.com>
2024-09-19 10:58:06 -0300

    19-09

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// Recuperações de parametros de data/hora do HTML
+// Recuperações de parametros de data/hora/DiaSemana do HTML
 
 const diaSemana = document.getElementById("dia-semana");
 const diaMesAno = document.getElementById("dia-mes-ano");
@@ -29,7 +29,7 @@ function daySemana () {
 function atualizaHora(){
     horaMinSeg.textContent = horaCompleta();
 }
-
+atualizaHora();
 setInterval(atualizaHora, 1000);
 
 diaMesAno.textContent = dataCompleta();
@@ -45,8 +45,20 @@ const dialogPonto = document.getElementById("dialog-ponto");
 
 const btnRegistrarPonto = document.getElementById("btn-registrar-ponto");
 btnRegistrarPonto.addEventListener("click", () => {
+    
+    let dialogSelect = document.getElementById("select-tipo-ponto");
+    let ultimoPonto = localStorage.getItem("tipoUltimoPonto");
+    dialogSelect.value = proxPonto[ultimoPonto];
+
     dialogPonto.showModal ();
 });
+
+let proxPonto = {
+    "entrada": "intervalo",
+    "intervalo": "volta-intervalo",
+    "volta-intervalo": "saida",
+    "saida": "entrada"
+}
 
 const btnDialogFechar = document.getElementById("btn-dialog-fechar");
 btnDialogFechar.addEventListener("click", () => {
@@ -55,10 +67,16 @@ btnDialogFechar.addEventListener("click", () => {
 // Parametros para Localização.
 
 navigator.geolocation.getCurrentPosition((position) => {
+    console.log(position);
+    console.log(position.coords.latitude);
+    console.log(position.coords.longitude);
 });
 
 
+
 //Parametros de Registro de Ponto
+
+
 
 const btnDialogRegistrarPonto = document.getElementById("btn-dialog-registrar-ponto");
 btnDialogRegistrarPonto.addEventListener("click", () => {
@@ -74,10 +92,38 @@ btnDialogRegistrarPonto.addEventListener("click", () => {
         "id" : 1
     }
 
-    localStorage.setItem("registro", JSON.stringify(Ponto));
+
+
+    salvarRegistroLocalStorage(Ponto);
+    localStorage.setItem("tipoUltimoPonto", TipoPonto);
 
     console.log(Ponto);
+    dialogPonto.close();
 });
+
+function salvarRegistroLocalStorage(Ponto) {
+localStorage.setItem("registro", JSON.stringify(Ponto));
+
+}
+
+
+function recuperaPontosLocalStorage(){
+    let todosOsPontos = localStorage.getItem("registro");
+    
+    if(!todosOsPontos) {
+        return [];
+    }
+
+    return JSON.parse(todosOsPontos);
+}
+
+function salvarRegistroLocalStorage(ponto) {
+    let pontos = recuperaPontosLocalStorage();
+
+    pontos.push(ponto);
+
+    localStorage.setItem("registro", JSON.stringify(pontos));
+}
 
 
 
